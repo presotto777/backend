39ab61246a39a429a3b6286e87547b5edb4d43c1
presotto777 <presotto.joao@escola.pr.gov.br>
2025-03-26 13:51:27 +0000

    aula01Variaveis

---
+++ src/main/java/com/mycompany/aula01Variaveis.java
@@ -0,0 +1,15 @@
+package com.mycompany;
+
+public class aula01Variaveis {
+
+    public static void main(string[] args) {
+        // variavel lógica
+
+        // tipo primitivo
+        boolean valorLogicoPrimitivo = true;
+
+        // Topo abstrato
+        Boolean valorLogicoAbstrato = false;
+
+    }
+}
