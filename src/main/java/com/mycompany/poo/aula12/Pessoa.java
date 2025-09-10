6b1a1de4028c456a62eacc4961800c6aac4d12ed
presotto777 <presotto.joao@escola.pr.gov.br>
2025-09-10 19:38:05 +0000

    aula12 contrutores pt2

--- src/main/java/com/mycompany/poo/aula12/Pessoa.java
+++ src/main/java/com/mycompany/poo/aula12/Pessoa.java
@@ -2,34 +2,49 @@ package com.mycompany.poo.aula12;
 
 public class Pessoa {
     /**
-     * Construtores 
-     * 
-     *  Utilizamos o método construtor para inicializar nossas classes,
-     * funcionando com uma configuração previa da mesma.
-     * 
-     * toda classe quando é criada, possui um método coNtrutor vazio
+     * Construtores
+     *
+     * Utilizamos o método construtor para inicializar nossas classes,
+     * funcionando com uma configuração prévia da mesma.
+     *
+     * Toda classe quando é criada, possui um método construtor vazio
      * que é chamado no momento que instanciamos a mesma.
+     * O texto na imagem diz o seguinte:
      * 
-     * uma mesma classe pode conter mais de um contrutor,
-     * porem cada construtor deve possuir quantidade
-     * e tipos de parametros diferentes
+     * Uma mesma classe pode conter mais de um construtor,
+     * porém cada construtor deve possuir quantidade
+     * e tipos de parâmetros diferentes.
      */
     private String nome;
     private int idade;
     private double altura;
     private double peso;
 
-    // Contrutor padrao
+    // Conatrutor padrão
     // Todo método construtor possui o mesmo nome da classe
     public Pessoa() {
-        System.out.println("Chamando o construtor padrão");
+        System.out.println("Chamada o construtor padrão");
     }
 
-    prublic Pessoa(String nome) {
+    public Pessoa(String nome) {
         this.nome = nome;
-        System.out.println("Chamando construtor 2");
+        System.out.println("Chamada construtor 2");
     }
 
+    public Pessoa(int idade) {
+        this.idade = idade;
+        System.out.println("Chamando construtor 3");
+    }
+
+    public Pessoa(double altura) {
+        this.altura = altura;
+        System.out.println("Chamando construtor 4");
+    }
+
+    // Não pode ter a mesma quantidade de parâmetros e tipos
+    // public Pessoa(double peso) {
+    // this.peso = peso;
+    // }
 
     public String getNome() {
         return nome;
@@ -37,6 +52,7 @@ public class Pessoa {
 
     public void setNome(String nome) {
         this.nome = nome;
+
     }
 
     public int getIdade() {
@@ -47,21 +63,19 @@ public class Pessoa {
         this.idade = idade;
     }
 
-    public double getAltura() {
-        return altura;
-    }
-
     public void setAltura(double altura) {
         this.altura = altura;
     }
 
-    public double getPeso() {
-        return peso;
+    public double getAltura() {
+        return altura;
     }
 
     public void setPeso(double peso) {
         this.peso = peso;
     }
 
-
+    public double getPeso() {
+        return peso;
+    }
 }
