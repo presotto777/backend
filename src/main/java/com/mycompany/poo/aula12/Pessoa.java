8ab5efc34f5ad7f4425a95357dedae042268b61d
presotto777 <presotto.joao@escola.pr.gov.br>
2025-09-19 14:35:08 +0000

    Aula 12, construtores e sobrecarga

--- src/main/java/com/mycompany/poo/aula12/Pessoa.java
+++ src/main/java/com/mycompany/poo/aula12/Pessoa.java
@@ -1,37 +1,21 @@
 package com.mycompany.poo.aula12;
 
 public class Pessoa {
-    /**
-     * Construtores
-     *
-     * Utilizamos o método construtor para inicializar nossas classes,
-     * funcionando com uma configuração prévia da mesma.
-     *
-     * Toda classe quando é criada, possui um método construtor vazio
-     * que é chamado no momento que instanciamos a mesma.
-     * O texto na imagem diz o seguinte:
-     * 
-     * Uma mesma classe pode conter mais de um construtor,
-     * porém cada construtor deve possuir quantidade
-     * e tipos de parâmetros diferentes.
-     */
     private String nome;
     private int idade;
     private double altura;
     private double peso;
 
-    // Conatrutor padrão
-    // Todo método construtor possui o mesmo nome da classe
-    public Pessoa() {
-        System.out.println("Chamada o construtor padrão");
-    }
+   public Pessoa() {
+    System.out.println("Chamando o construtor padrão");
+   }
 
-    public Pessoa(String nome) {
-        this.nome = nome;
-        System.out.println("Chamada construtor 2");
-    }
+   public Pessoa(String nome) {
+    this.nome = nome;
+    System.out.println("chamando construtor 2 ");
+   }
 
-    public Pessoa(int idade) {
+   public Pessoa(int idade) {
         this.idade = idade;
         System.out.println("Chamando construtor 3");
     }
@@ -43,8 +27,16 @@ public class Pessoa {
 
     // Não pode ter a mesma quantidade de parâmetros e tipos
     // public Pessoa(double peso) {
-    // this.peso = peso;
-    // }
+    //    this.peso = peso;
+    //}
+
+    public Pessoa(String nome, double altura){
+        this.nome=nome;
+        this.altura=altura;
+        System.out.println("chamando construtor 5 ");
+
+        
+    }
 
     public String getNome() {
         return nome;
@@ -52,30 +44,30 @@ public class Pessoa {
 
     public void setNome(String nome) {
         this.nome = nome;
-
     }
 
     public int getIdade() {
         return idade;
     }
 
-    public void setIdade(int idade) {
+    public void setIdade(int nome) {
         this.idade = idade;
-    }
-
-    public void setAltura(double altura) {
-        this.altura = altura;
     }
 
     public double getAltura() {
         return altura;
     }
 
-    public void setPeso(double peso) {
-        this.peso = peso;
+    public void setAltura(double altura) {
+        this.altura = altura;
     }
 
     public double getPeso() {
         return peso;
     }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
 }
